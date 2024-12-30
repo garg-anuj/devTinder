18381310025b19299538083c5a6c7ee1d7ad0023
garg-anuj <anujgarg2018@gmail.com>
2024-12-30 22:01:31 +0530

    next()

--- src/app.js
+++ src/app.js
@@ -13,6 +13,40 @@ const PORT = 3000;
 
 const app = express();
 
+// !-----------------commit-3 next()  11:00-20:00----------------------------------------------
+
+/* ek baar data client pr pauch gya fir hm dubara send kr rhe hai to wo
+ use change nhi kar skta connection break hoo jata hai yeah error dikhayega
+ lekin baki ke function backend pr chlenge client pr response send krne ke baad */
+
+// app.use(
+//   "/user",
+//   (req, res, next) => {
+//     next();
+//     res.send("ab data dikha 1");
+//   },
+//   (req, res) => res.send("ab data dikha 2"),
+//   (req, res) => {}
+// );
+
+// ek baar client ko request send krdi toh wo change nhi kar skte
+
+// app.use(
+//   "/user",
+//   (req, res, next) => next(),
+//   (req, res) => res.send("ab data dikha"),
+//   (req, res) => {}
+// ); // yeah next () ki wje se dusre handler kaa response show kr dega
+
+// app.use(
+//   "/user",
+//   (req, res) => {},
+//   (req, res) => res.send("handler 2 response"),
+//   (req, res) => res.send("handler 3 response")
+// ); // yeah again infinite saa chlta rhega res nhi dega koi req hang hoo jati hai
+
+//
+
 // !-----------------commit-2 Multiple Handlers 7:00 - 11:00-----------------------------------
 
 // we can add multiple handlers inside it
