670fd267c2a38b127f6be36706f3a8a818a4abd2
garg-anuj <anujgarg2018@gmail.com>
2024-12-30 06:51:21 +0530

    Advanced Routing Concept using  regex in routes

--- homework.md
+++ homework.md
@@ -10,3 +10,18 @@
 - What is the use of "-g" while npm install
 - Difference between ^ and ~ 
 -  Why Npm install
+
+
+<!-- Ep-4 -->
+initialized git 
+.gitignore
+Create a remote repo on githup
+Push all code to remote origin
+Play with routes and route extension ex: /hello, / , hello/2, /xyz
+Order of the routes matter a lot
+Install Postman app make a workspace/collection > test API call
+Write logic to handle GET, POST , DELETE API calls and test them on postman
+Explore routing and use of ?, +, (), * in the routes
+Use of regex in routes /a/, /.*fly$/
+Reading the query params in the routes
+Reading the dynamic  routes
--- src/app.js
+++ src/app.js
@@ -2,26 +2,46 @@ const express = require("express");
 const PORT = 3000;
 
 const app = express();
+const userObj = {
+  name: "Anuj",
+  lastName: "Garg",
+};
+// ------comment-3-Advanced Routing Concept using  regex in routes------------------------------------
+// app.get("/ab?c", (req, res) => res.send(userObj));
+// app.get("/ab+c", (req, res) => res.send(userObj));
+// app.get("/ab*cd", (req, res) => res.send(userObj));
+// app.get("/a(bc)?d", (req, res) => res.send(userObj));
+// app.get("/a(bc)+d", (req, res) => res.send(userObj));
+// app.get("/ae(bc)?hd", (req, res) => res.send(userObj));
+// app.get("anyRegex", (req, res) => res.send(userObj));
+// app.get("/a/", (req, res) => res.send(userObj));
+// app.get("/.*fly$/", (req, res) => res.send(userObj));
+
+app.get("/user/:userId/:name", (req, res) => {
+  console.log(req.params, req.query);
+  res.send(userObj);
+});
+app.get("/user", (req, res) => res.send(userObj));
 
 // ------comment-2------------------------------------
 
-app.use("/user", (req, res) => res.send("show anything"));
+// app.use("/user", (req, res) => res.send("show anything"));
 
-//This will only handle GET call to /user
-app.get("/user", (req, res) =>
-  res.send({
-    name: "Anuj",
-    lastName: "Garg",
-  })
-);
-app.post("/user", (req, res) =>
-  res.send("Data Successfully saved to database")
-);
+// //This will only handle GET call to /user
+// app.get("/user", (req, res) =>
+//   res.send({
+//     name: "Anuj",
+//     lastName: "Garg",
+//   })
+// );
+// app.post("/user", (req, res) =>
+//   res.send("Data Successfully saved to database")
+// );
 
-app.delete("/user", (req, res) => res.send("Deleted Successfully"));
+// app.delete("/user", (req, res) => res.send("Deleted Successfully"));
 
-// this will match all the HTTP method API calls /test
-app.use("/test", (req, res) => res.send("Test"));
+// // this will match all the HTTP method API calls /test
+// app.use("/test", (req, res) => res.send("Test"));
 
 // ------comment-1------------------------------------
 // // app.use("/", (req, res) => {
