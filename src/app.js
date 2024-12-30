bcd578cac916a3409006cc3383d5ae3f350e2192
garg-anuj <anujgarg2018@gmail.com>
2024-12-30 22:00:58 +0530

    Multiple Handlers

--- src/app.js
+++ src/app.js
@@ -13,16 +13,30 @@ const PORT = 3000;
 
 const app = express();
 
-// -------------------commit-1  Empty routeHandler 4:00 -------------------------------------------------
+// !-----------------commit-2 Multiple Handlers 7:00 - 11:00-----------------------------------
+
+// we can add multiple handlers inside it
+// / first will run first  baki ki run nhi honge
+
+// app.use(
+//   "/user",
+//   (req, res) => res.send("handler 1 response"),
+//   (req, res) => res.send("handler 2 response"),
+//   (req, res) => res.send("handler 3 response")
+// );
+
+//
+
+// !-----------------commit-1  Empty routeHandler 4:00 ----------------------------------------
 
 /* if we did not send any  response  we keep this Empty routeHandler 
 then it will go infinite tk chlta rhega yeah kuch time tk  */
 
-app.use(
-  "/user",
-  (req, res) => {},
-  (req, res) => {}
-);
+// app.use(
+//   "/user",
+//   (req, res) => {},
+//   (req, res) => {}
+// );
 
 app.listen(PORT, () => {
   console.log("server successfully  listening on the port " + PORT);
