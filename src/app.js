b5bc87e68237d8f5f82f33c888ea7592d1baef52
garg-anuj <anujgarg2018@gmail.com>
2024-12-30 06:13:01 +0530

    exploring GET, POST, DELETE

--- src/app.js
+++ src/app.js
@@ -3,22 +3,43 @@ const PORT = 3000;
 
 const app = express();
 
-// app.use("/", (req, res) => {
-//   console.log("chla ab chla");
-//   res.send("helllo world use");
-// });
+// ------comment-2------------------------------------
 
-// joh bhi test ke baad ayega usme yeah a..use sbko overWrite kar dega
-//  TEST response me milega
+app.use("/user", (req, res) => res.send("show anything"));
+
+//This will only handle GET call to /user
+app.get("/user", (req, res) =>
+  res.send({
+    name: "Anuj",
+    lastName: "Garg",
+  })
+);
+app.post("/user", (req, res) =>
+  res.send("Data Successfully saved to database")
+);
+
+app.delete("/user", (req, res) => res.send("Deleted Successfully"));
+
+// this will match all the HTTP method API calls /test
 app.use("/test", (req, res) => res.send("Test"));
-app.use("/hello", (req, res) => res.send("hello"));
-app.use("/hello/2", (req, res) => res.send("hello hello 2"));
-app.use("/", (req, res) => res.send("Home Page /"));
 
-app.get("/new", (req, res) => {
-  console.log("chla ab chla");
-  res.send("new worldd get");
-});
+// ------comment-1------------------------------------
+// // app.use("/", (req, res) => {
+// //   console.log("chla ab chla");
+// //   res.send("helllo world use");
+// // });
+
+// // joh bhi test ke baad ayega usme yeah a..use sbko overWrite kar dega
+// //  TEST response me milega
+// app.use("/test", (req, res) => res.send("Test"));
+// app.use("/hello", (req, res) => res.send("hello"));
+// app.use("/hello/2", (req, res) => res.send("hello hello 2"));
+// app.use("/", (req, res) => res.send("Home Page /"));
+
+// app.get("/new", (req, res) => {
+//   console.log("chla ab chla");
+//   res.send("new worldd get");
+// });
 
 app.listen(PORT, () => {
   console.log("server successfully  listening on the port " + PORT);
