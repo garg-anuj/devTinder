d2841a54b2930f229159f6720aa835709b10b78f
garg-anuj <anujgarg2018@gmail.com>
2024-12-30 05:38:49 +0530

    app.use practice

--- src/app.js
+++ src/app.js
@@ -3,18 +3,21 @@ const PORT = 3000;
 
 const app = express();
 
-app.use((req, res) => {
-  console.log("chla ab chla");
-  res.send("helllo world use");
-});
+// app.use("/", (req, res) => {
+//   console.log("chla ab chla");
+//   res.send("helllo world use");
+// });
 
-app.use("/test", (req, res) => {
-  res.send("Test");
-});
+// joh bhi test ke baad ayega usme yeah a..use sbko overWrite kar dega
+//  TEST response me milega
+app.use("/test", (req, res) => res.send("Test"));
+app.use("/hello", (req, res) => res.send("hello"));
+app.use("/hello/2", (req, res) => res.send("hello hello 2"));
+app.use("/", (req, res) => res.send("Home Page /"));
 
-app.get("/", (req, res) => {
+app.get("/new", (req, res) => {
   console.log("chla ab chla");
-  res.send("hello worldd get");
+  res.send("new worldd get");
 });
 
 app.listen(PORT, () => {
