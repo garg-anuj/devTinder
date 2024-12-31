a78db32fcae5ebbdfd287aeedbe4d2650251ca7d
garg-anuj <anujgarg2018@gmail.com>
2024-12-31 23:05:15 +0530

    Post and get Method

--- src/app.js
+++ src/app.js
@@ -4,22 +4,31 @@ const app = express();
 const { connectDB } = require("./config/database");
 const User = require("./models/user");
 
-app.post("/signup", async (req, res) => {
-  const userObj = {
-    firstName: "Anuj",
-    lastName: "Garg",
-    emailId: "anuj@gmail.com",
-    password: 12345678,
-    age: 23,
-    gender: "male",
-  };
+app.use(express.json());
 
+app.post("/signup", async (req, res) => {
+  const userObj = req.body;
   try {
     const user = new User(userObj); //creating a new instance of user model
     await user.save();
     res.send(user);
   } catch (err) {
     res.status(400).send("Error Saving the user " + err.message);
+  }
+});
+// !--------------------------- 26:00 - 36:00---------------------------------
+app.get("/users", async (req, res) => {
+  // const emailId = req.body.emailId
+  try {
+    // const users = await User.findOne(req.body);
+    // const users = await User.findOne({emailId});
+    const users = await User.find({});
+    if (users.length === 0) {
+      res.status(404).send("No users found");
+    }
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Error get users " + err.message);
   }
 });
 
